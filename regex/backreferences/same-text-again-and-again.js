dc77bb580a52983580d5bdbf41291cdb53954316
jashanbhullar <jsbhullar96@gmail.com>
2019-04-02 17:31:35 +0530

    Regex: Backreferences done

---
+++ regex/backreferences/same-text-again-and-again.js
@@ -0,0 +1,18 @@
+var Regex_Pattern = /^([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10$/; //Do not delete '/'. Replace __________ with your regex.
+
+function processData(Test_String) {
+  //Enter your code here
+
+  console.log(!!Test_String.match(Regex_Pattern));
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function(input) {
+  _input += input;
+});
+
+process.stdin.on("end", function() {
+  processData(_input);
+});
